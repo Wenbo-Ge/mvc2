11c3be3c028bb5480b1cca06eed6d367382784d6
Wenbo-Ge <wge19920921@gmail.com>
2018-03-30 00:04:44 +0000

    second commit

--- wenboShop-database.php
+++ wenboShop-database.php
@@ -1,1 +1,30 @@
-wenboShop.php
+<?php
+
+class DBconnection {
+	private $conn;
+
+	private function getConnInstance(){
+		if (!$conn) {
+			$this->conn=new PDO('mysql:host=localhost;dbname=Shop;charset=utf8mb4','root','root');
+		}
+		return $this->conn;
+	}
+
+	public function getAllItemsReturnArr(){
+		$this->getConnInstance();
+		$stmt=$this->conn->query('SELECT * FROM item');
+		$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $result;
+	}
+
+	public function getAllItemsReturnObj(){
+		
+	}
+}
+
+
+
+
+
+
+?>
